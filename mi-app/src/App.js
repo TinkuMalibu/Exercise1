cef1c1c466a18a3bbcee6dbb7854d6f26685df97
cecilia reynoso <cecireynoso@gmail.com>
2023-11-21 22:49:26 -0300

    props

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>This is what our cute catpreneurs say</h1>
-        <Testimonio/>
+        <Testimonio
+        nombre='Katie Reynoso'
+        pais='Argentina'
+        imagen=''
+        cargo='Feline entrepeneur'
+        empresa='Human toys for cats'
+        testimonio='In the quiet neighborhood of Catville, I set out to chase a bold dream: to become the owner of the best cat toy store! With my endearing gaze and a persuasive meow, I convinced my fellow feline friends to buy my exclusive toys. My business grew rapidly, opening branches all over the place. Cats went crazy for my quality products and unique designs. Not only did I triumph as a feline entrepreneur, but I also became an inspiration for other catpreneurs. My toy empire became a global success, and Im purring with happiness!.'/>
       </div>
     </div>
   );
--- mi-app/src/componentes/Testimonio.jsx
+++ mi-app/src/componentes/Testimonio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../stylesheets/Testimonio.css';
 
-  function Testimonio() {
+  function Testimonio(props) {
    return (
     <div className= 'contenedor-testimonio'>
       <img
@@ -9,9 +9,9 @@ import '../stylesheets/Testimonio.css';
         src={require('../imagenes/cat1.png')}
         alt='foto de un gatito cute'/>
       <div className="contenedor-texto-testimonio">
-        <p className='nombre-testimonio'>Miu Miu</p>
-        <p className='cargo-testimonio'>Entrepreneur in My HUMAN TOYS</p>
-        <p className='texto-testimonio'>In the quiet neighborhood of Catville, I set out to chase a bold dream: to become the owner of the best cat toy store! With my endearing gaze and a persuasive meow, I convinced my fellow feline friends to buy my exclusive toys. My business grew rapidly, opening branches all over the place. Cats went crazy for my quality products and unique designs. Not only did I triumph as a feline entrepreneur, but I also became an inspiration for other catpreneurs. My toy empire became a global success, and I'm purring with happiness!</p>
+        <p className='nombre-testimonio'>{props.nombre} from {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} in {props.empresa}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     </div>
    );
